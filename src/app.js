fff6591066aa5b3799ce2542464465f0bcedbbd7
Juan Jose Bravo Quiroga <juanjo.bravo3@gmail.com>
2025-01-27 02:59:00 +0000

    domainGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let subDomainArray = ["www.", "blog.", "web.", "mail."];
+let domainArray1 = ["Not", "The", "My", "Space"];
+let domainArray2 = [
+  "Metube",
+  "FantasyFlux",
+  "NebulaNexus",
+  "Netshflix",
+  "RandomDomain",
+  "TokTik",
+  "4Geeks",
+  "Pear"
+];
+let extensionArray = [".com", ".net", ".us", ".io", ".es", ".co", ".ch"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let subdomain = Math.floor(Math.random() * subDomainArray.length);
+let domain1 = Math.floor(Math.random() * domainArray1.length);
+let domain2 = Math.floor(Math.random() * domainArray2.length);
+let extension = Math.floor(Math.random() * extensionArray.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function domainGenerated() {
+  return (
+    "https://" +
+    subDomainArray[subdomain] +
+    domainArray1[domain1] +
+    domainArray2[domain2] +
+    extensionArray[extension]
+  );
+}
+
+document.querySelector("#domain").innerHTML = domainGenerated();
